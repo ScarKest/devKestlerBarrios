8e3bbb84b79722a22c310be6abb0f4a34ab4e691
ScarKest <luiseduardo3213@gmail.com>
2022-05-19 01:14:33 -0600

    Creacion de avatar circular

--- lib/home.dart
+++ lib/home.dart
@@ -6,23 +6,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: ListView(
         padding: const EdgeInsets.all(50),
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "lib/assets/images/dash.png",
-                width: size.width * 0.3,
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(width: 50),
-              name(context)
-            ],
-          )
+          name(context),
+          const SizedBox(height: 100),
+          about(context),
         ],
       ),
     );
@@ -35,14 +25,47 @@ Widget name(BuildContext context) {
     color: Colors.white,
     fontWeight: FontWeight.bold,
   );
-  return SizedBox(
-    width: 50.w,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text("Kestler Barrios", style: style),
-        Text("Flutter Developer", style: style),
-      ],
-    ),
+  var size = MediaQuery.of(context).size;
+
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Image.asset(
+        "lib/assets/images/dash.png",
+        width: size.width * 0.3,
+        fit: BoxFit.cover,
+      ),
+      const SizedBox(width: 50),
+      SizedBox(
+        width: 50.w,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Kestler Barrios", style: style),
+            Text("Flutter Developer", style: style),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget about(BuildContext context) {
+  var style = TextStyle(
+    fontSize: 5.sp,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+  );
+  return Row(
+    children: [
+      SizedBox(
+        height: MediaQuery.of(context).size.height * 0.3,
+        width: MediaQuery.of(context).size.width * 0.3,
+        child: const CircleAvatar(
+          backgroundImage: AssetImage("lib/assets/images/me.jpeg"),
+        ),
+      ),
+      Text("Sobre Mi: Programador del framework Flutter", style: style)
+    ],
   );
 }
